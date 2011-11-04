46b5ab842a383b76e3c3d2a7ef7b40f385cc7139
Alexey <tanlogan@mail.ru>
2011-11-05 01:21:10 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Probnick::Application.config.secret_token = 'd47de3915424966b2080edcd62cca314e2d874e0123d074897d7fda787cf91ec6c344415461818156eaf13e8ba9ad245cad8ee42c29a29fb8f66e30cb3260fde'
